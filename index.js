38fe52393cf8c72ca6e4f7f66b5a2d87b8869575
thellecodes <boltdsg@gmail.com>
2023-03-11 00:34:45 +0300

    setup

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import App from "./src/App";
 
-// const container = document.getElementById("app");
-
-// const root = createRoot(container);
-// root.render(<App />);
-
-ReactDOM.createRoot(document.getElementById("app")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
